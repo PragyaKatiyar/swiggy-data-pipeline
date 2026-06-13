ac7d77f36c6e35bccc74b5818ef2cd642daec0ab
Pragya Katiyar <104066521+PragyaKatiyar@users.noreply.github.com>
2026-06-13 12:38:27 +0530

    Delete notebooks/01_bronze_orders.py

--- notebooks/01_bronze_orders.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-import pyspark.sql.types
-import pyspark.sql.functions
-import sys
-import config
-
-spark = SparkSession.builder \
-    .appName("Swiggy_Pipeline") \
-    .getOrCreate()
